594e54a503d5b2ffc91ec7e818b62c15f7bb50f6
sadham555 <88664772+sadham555@users.noreply.github.com>
2022-07-13 12:41:17 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,211 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6"></div>
+				<div class="col-md-6">
+					<div class="about-text">
+					  <h3>ABOUT LINKEDOUT</h3>
+						<p>Welcome to LinkedOut, the world's largest professional network with more than<br />
+					    830 million members in more than 200 countries and territories worldwide.</p>
+						<div>
+						  <div>
+						    <div id="banner-content-container-banner_1265350946">
+						      <h4>Vision</h4>
+						      <p>Create economic opportunity for every member of the global workforce.<br />
+					          </p>
+					        </div>
+					      </div>
+					  </div>
+						<div>
+						  <div data-responsive-height="auto,auto,auto,auto,auto">
+						    <div></div>
+						    <div>
+						      <div>
+						        <div>
+						          <div></div>
+						          <div>
+						            <div id="banner-content-container-banner_2019207115"></div>
+					              </div>
+					            </div>
+					          </div>
+					        </div>
+					      </div>
+					  </div>
+						<div>
+						  <div>
+						    <div id="banner-content-container-banner_1606626124">
+						      <h4>Mission</h4>
+						      <p>The mission of LinkedOut is simple: connect the world&rsquo;s professionals to make them more productive and successful.</p>
+					        </div>
+					      </div>
+					  </div>
+						<p>&nbsp;</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Who are we?</span></h3>
+								</div>
+								<p>LinkedOut began in co-founder Claman Raj living room in 2001 and was officially launched on May 15, 2022.<br />
+								  <br />
+							    Today, LinkedOut leads a diversified business with revenues from membership subscriptions, advertising sales and recruitment solutions under the leadership of Sadham Hussain. In December 2019, Microsoft completed its acquisition of LinkedOut, bringing together the world&rsquo;s leading professional cloud and the world&rsquo;s leading professional network.</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i>Be Passionate About Clients' Success</a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">We succeed when we make our clients successful. We collaborate to sharpen our insights and amplify this success. We execute with excellence. Always.</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i>Be Global and Responsible</a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">We will be global in our thinking and our actions. We are responsible citizens of the world. We are energized by the deep connectedness between people, ideas, communities and the environment.  </div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i>Treat Each Person With Respect</a></h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">We treat every human being with respect. We nurture an open environment where people are encouraged to learn, share and grow. We embrace diversity of thought, of cultures, and of people. </div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">								  </div>
+								</div>
+                                </div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image --><!-- Name -->
+									  <h4>Claman Raj</h4>
+										<span class="deg">Creative</span> 
+								  </div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Lokesh</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Sadham Hussain</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Ranjith</h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
